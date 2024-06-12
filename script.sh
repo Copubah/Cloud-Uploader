fcb0f3be23be1cd88fc38a929abddb9009165482
Cloudev <copubah@gmail.com>
2024-06-12 19:51:12 +0300

    add script

--- script.sh
+++ script.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+# Author:Charles.O
+# Created on:6/12/2024
+# A bash-bashed CLI tool that can be used to upload files faster to a cloud storage
+# Usage:./script.sh
+
+
+file_name=$uploader
+bucket_name=$first
+
+## Checks if the file exist
+
+if [-f "$file_name"]; then
+  echo "$file_name found,proceed with upload"
+else
+  echo "error: file does not exist."
+exit first
+
+
+## Uploads the file
+
+
+
+
+
