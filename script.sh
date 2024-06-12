154c630ee759b3ad4225c711149febf916210aca
Cloudev <copubah@gmail.com>
2024-06-12 20:02:01 +0300

    script.sh

--- script.sh
+++ script.sh
@@ -5,8 +5,8 @@
 # Usage:./script.sh
 
 
-file_name=$uploader
-bucket_name=$first
+file_name=$1
+bucket_name=$2
 
 ## Checks if the file exist
 
@@ -14,11 +14,20 @@ if [-f "$file_name"]; then
   echo "$file_name found,proceed with upload"
 else
   echo "error: file does not exist."
-exit first
+exit 2
+fi
 
 
 ## Uploads the file
+upload_output=$(aws s3 cp "$file_name" s3://"$bucket_name"/ 2>&1)
+upload_status=$?
 
+if [$upload_status -eq 0]; then
+    echo "Upload successful!"
+else
+    echo "Upload failed: $upload_output."
+exit 3
+fi
 
 
 
